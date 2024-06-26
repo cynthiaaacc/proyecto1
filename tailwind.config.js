07d9440901283986625cfb6fd1ea28eb1817efbc
Cynthia <cynthia@Cynthia>
2024-06-27 00:49:34 +0200

    darkmodetext

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Inter'], 
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
